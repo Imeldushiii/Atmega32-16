1ebf08a8ecc8cbbdded2b14172266161b8de5e4d
Imeldushiii <91779600+Imeldushiii@users.noreply.github.com>
2023-07-24 00:44:14 +0200

    Update przerwania.cpp

--- przerwania.cpp
+++ przerwania.cpp
@@ -14,8 +14,8 @@ ISR(INT0_vect) {
 
 int main() {
 	MCUCR |= (1 << ISC00) | (1 << ISC01);
-	GICR |= (1 << INT0); //JE¯ELI PODEPNIEMY INT0 do masy albo VCC to wylaczy siê ISR()
-	
+	GICR |= (1 << INT0); //JEZELI PODEPNIEMY INT0 do masy albo VCC to wylaczy siê ISR()
+	//Gdy zamiast INT0 damy do INT1 to gdy pojawi sie VCC to wtedy wylaczy sie ISR()
 	sei(); // inicjuje przerwania
 	DDRA = 0xFF;
 	while(1) {
